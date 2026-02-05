5824b9f729cc267534f0b3fd7107b78aab4d91d0
Virtual Sense <virtual_sense@naver.com>
2026-02-05 14:45:22 +0900

    quiz5
    Fix the condition to check for odd numbers.

---
+++ C/quiz/quiz5.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int main()
+{
+	int number;
+	printf("Number : ");
+	scanf("%d", &number);
+
+	if (number % 2 == ) {
+		printf("홀수\n");
+	}
+	else {
+		printf("짝수\n");
+	}
+
+	return 0;
+}
