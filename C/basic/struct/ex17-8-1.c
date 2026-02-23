ba88e481f8635746b36f849030e45b634aeae9db
Virtual Sense <virtual_sense@naver.com>
2026-02-23 15:55:26 +0900

    DB 참조용으로 간단하게 만들 때  char* name

---
+++ C/basic/struct/ex17-8-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct person
+{
+   int id;
+   //char name[20];
+   char* name; 
+   int age;
+   char* tel;
+};
+int main()
+{
+    struct person list[2] = {
+      {1, "홍길동", 20, "010-1111-1111"},
+      {2, "이순신", 45, "010-2222-2222"}
+    };
+
+    for(int i=0; i<2; i++) {
+      printf("%d\t %10s\t %d\t %15s\n", 
+        list[i].id, 
+        list[i].name, 
+        list[i].age, 
+        list[i].tel);
+    }
+    return 0;
+}
